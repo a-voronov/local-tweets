6cd97159e540fd8cb63124e62646f2f20f19015e
Aleksandr Voronov <voronovaleksandr91@gmail.com>
2015-01-18 23:46:26 +0200

    Created LocalTweets Storyboard Blueprint and initial Project Structure

---
+++ LocalTweets/Classes/Presentation/LocalTweetsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocalTweetsViewController.h
+//  LocalTweets
+//
+//  Created by Alexander Voronov on 18/1/15.
+//  Copyright (c) 2015 Alexander Voronov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocalTweetsViewController : UIViewController
+
+@end
---
+++ LocalTweets/Classes/Presentation/LocalTweetsViewController.m
@@ -0,0 +1,37 @@
+//
+//  LocalTweetsViewController.m
+//  LocalTweets
+//
+//  Created by Alexander Voronov on 18/1/15.
+//  Copyright (c) 2015 Alexander Voronov. All rights reserved.
+//
+
+#import "LocalTweetsViewController.h"
+
+@interface LocalTweetsViewController ()
+
+@end
+
+@implementation LocalTweetsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
